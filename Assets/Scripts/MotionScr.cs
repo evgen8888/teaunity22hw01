f1a27996f30473b96b82509849c7e98c566b600e
evgen <evgenkriv.uk@gmail.com>
2022-02-13 20:56:29 +0200

    first commit

---
+++ Assets/Scripts/MotionScr.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionScr : MonoBehaviour
+{
+    //[SerializeField] private Transform _terget;
+    [SerializeField] private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Movement(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Movement(Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Movement(Vector3.back);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Movement(Vector3.right);
+        }
+    }
+
+    private void Movement(Vector3 direction)
+    {
+        transform.position = transform.position + direction * _speed;
+    }
+}
